8aab87bb711d395edb09d31b204e23b659b6d6b2
Danor93 <danors@gmail.com>
2023-09-02 18:49:02 +0300

    add database class and update the RESTuser to get users and connect to MongoDB

--- Infastucture/RESTUser.py
+++ Infastucture/RESTUser.py
@@ -1,41 +1,51 @@
 from flask import Flask, request, jsonify
 from flask_pymongo import PyMongo, ObjectId
+from flask_cors import CORS
+from database import database
+
+dbase = database()
+db = dbase.db
+
+# Access a collection named 'users'
+users = db['users']
+
+# Insert a document into the 'users' collection
+# users.insert_one({"name": "John", "email": "john@example.com"})
 
 app = Flask(__name__)
-
-# MongoDB configuration
-app.config["MONGO_URI"] = "mongodb://localhost:27017/mydatabase"
-mongo = PyMongo(app)
-
-# Reference to the students collection
-students = mongo.db.students
-
+CORS(app)
 
 @app.route('/users', methods=['GET'])
-def get_user():
+def get_users():
     return
 
+from flask import request, jsonify
 
-
-@app.route('/student', methods=['POST'])
+@app.route('/user/', methods=['POST'])
 def add_user():
+    # Get JSON data from the request
+    user_data = request.get_json()
+
+    # Insert the user data into the MongoDB collection
+    inserted = users.insert_one(user_data)
+
+    # Convert ObjectId to string
+    user_data["_id"] = str(inserted.inserted_id)
+
+    return jsonify({"message": "User added successfully", "user": user_data}), 201
+
+
+@app.route('/user/<id>', methods=['GET'])
+def get_user_by_id(id):
     return
 
-
-@app.route('/student/<id>', methods=['GET'])
-def get_user(id):
-    return
-
-
-@app.route('/student/<id>', methods=['PUT'])
+@app.route('/user/<id>', methods=['PUT'])
 def update_user(id):
     return
 
-
-@app.route('/student/<id>', methods=['DELETE'])
+@app.route('/user/<id>', methods=['DELETE'])
 def delete_user(id):
-    return jsonify({"message": "Student deleted successfully"})
-
+    return jsonify({"message": "User deleted successfully"})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='10.0.0.23', port=5000, debug=False)
